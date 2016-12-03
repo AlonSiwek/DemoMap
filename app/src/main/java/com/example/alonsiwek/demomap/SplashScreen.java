7e0682e1d64385b5232f3064b4f52d78e1f6e0cd
dor <dorshahar@gmail.com>
2016-12-03 20:48:35 +0200

    Splash screen was added

--- app/src/main/java/com/example/alonsiwek/demomap/SplashScreen.java
+++ app/src/main/java/com/example/alonsiwek/demomap/SplashScreen.java
@@ -10,15 +10,15 @@ import android.os.Handler;
  * done by: http://stackoverflow.com/questions/5486789/how-do-i-make-a-splash-screen
  */
 
-public class SplashScreen extends Activity {
-
-    private  final int SPLASH_DISPLAY_DURATION = 1000;
-
-    public  void onCreate(Bundle icicle){
-        super.onCreate(icicle);
-        setContentView();
-    }
-
-
-
-}
+//public class SplashScreen extends Activity {
+//
+//    private  final int SPLASH_DISPLAY_DURATION = 1000;
+//
+//    public  void onCreate(Bundle icicle){
+//        super.onCreate(icicle);
+//        setContentView();
+//    }
+//
+//
+//
+//}
